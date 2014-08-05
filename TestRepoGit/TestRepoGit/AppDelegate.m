dac2a04dfd9dabfeafaea78a54077ee841b37b87
Fernando Flores <fernando.flores@ironbit.net>
2014-08-05 15:16:20 -0500

    Se agrego pantalla de home

--- TestRepoGit/TestRepoGit/AppDelegate.m
+++ TestRepoGit/TestRepoGit/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "HomeViewController.h"
 
 @implementation AppDelegate
 
@@ -14,6 +15,10 @@
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
+    HomeViewController *home = [[HomeViewController alloc]init];
+    UINavigationController *nav = [[UINavigationController alloc]initWithRootViewController:home];
+    self.window.rootViewController = nav;
+    
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;
---
+++ TestRepoGit/TestRepoGit/HomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.h
+//  TestRepoGit
+//
+//  Created by Fernando Flores on 8/5/14.
+//  Copyright (c) 2014 Fernando Flores. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeViewController : UIViewController
+
+@end
