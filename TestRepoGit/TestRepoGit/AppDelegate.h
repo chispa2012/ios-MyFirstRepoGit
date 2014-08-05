fe2940a34d9fb7614a5b8cbeaab4c24f4fd4d60e
Fernando Flores <fernando.flores@ironbit.net>
2014-08-05 13:50:21 -0500

    My first commit

---
+++ TestRepoGit/TestRepoGit/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestRepoGit
+//
+//  Created by Fernando Flores on 8/5/14.
+//  Copyright (c) 2014 Fernando Flores. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
